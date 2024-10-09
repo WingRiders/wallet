d99b1e199ebfa7d35b24c93bfdd463191681e7a9
TechRiderWR <techrider@wingriders.com>
2024-10-09 12:41:22 +0000

    [GATEWAY/EXAMPLE] Change background color

--- example-app/src/main.tsx
+++ example-app/src/main.tsx
@@ -2,12 +2,14 @@ import {CssBaseline, ThemeProvider} from '@mui/material'
 import {StrictMode} from 'react'
 import {createRoot} from 'react-dom/client'
 import {App} from './App.tsx'
+import {AppBackground} from './components/AppBackground.tsx'
 import {theme} from './theme.ts'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <ThemeProvider theme={theme}>
       <CssBaseline />
+      <AppBackground />
       <App />
     </ThemeProvider>
   </StrictMode>,
