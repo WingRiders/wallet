aa60406aff5345922362e1ef80d9b800ec0ca8e2
TechRiderWR <techrider@wingriders.com>
2024-10-17 08:29:16 +0000

    [PLUGIN] Support multiple init modes

--- dapp-plugin/src/init/init.ts
+++ dapp-plugin/src/init/init.ts
@@ -2,28 +2,53 @@ import type {NetworkName} from '@wingriders/cab/types'
 import {DataApi} from '../dataApi/DataApi'
 import {WalletGateway} from '../gateway/WalletGateway'
 import {injectCborApi} from './inject'
+import {WalletInitMode} from './types'
 
 export type InitDappPluginOptions = {
   /**
    * URL to the wallet gateway application.
    */
   gatewayUrl: string
-  /**
-   * URL to the CAB backend server for each network.
-   */
-  cabServerUrlByNetwork: Record<NetworkName, string>
-}
+} & (
+  | {
+      mode: WalletInitMode.SINGLE_NETWORK
+      network: NetworkName
+      /**
+       * URL to the CAB backend server for the selected network.
+       */
+      cabServerUrl: string
+    }
+  | {
+      mode: WalletInitMode.MULTI_NETWORK
+      /**
+       * URL to the CAB backend server for each network.
+       */
+      cabServerUrlByNetwork: Record<NetworkName, string>
+    }
+)
 
 export const initDappPlugin = (options: InitDappPluginOptions) => {
   injectCborApi({
-    gateway: new WalletGateway({url: options.gatewayUrl}),
-    getDataApi: (network) =>
-      new DataApi({
-        cabServerUrl: options.cabServerUrlByNetwork[network],
-        network,
-      }),
     name: 'WingRiders',
     apiVersion: '0.0.1',
     icon: 'TODO',
+    gateway: new WalletGateway({url: options.gatewayUrl}),
+    ...(options.mode === WalletInitMode.SINGLE_NETWORK
+      ? {
+          mode: WalletInitMode.SINGLE_NETWORK,
+          network: options.network,
+          dataApi: new DataApi({
+            cabServerUrl: options.cabServerUrl,
+            network: options.network,
+          }),
+        }
+      : {
+          mode: WalletInitMode.MULTI_NETWORK,
+          getDataApi: (network) =>
+            new DataApi({
+              cabServerUrl: options.cabServerUrlByNetwork[network],
+              network,
+            }),
+        }),
   })
 }
---
+++ dapp-plugin/src/init/types.ts
@@ -0,0 +1,11 @@
+export enum WalletInitMode {
+  /**
+   * Use if your dApp is working with a single network.
+   * If user tries to connect with a wallet that is not on the selected network, the connection will be rejected.
+   */
+  SINGLE_NETWORK = 'single-network',
+  /**
+   * Use if your dApp is working with multiple networks based on the network from the connected wallet.
+   */
+  MULTI_NETWORK = 'multi-network',
+}
